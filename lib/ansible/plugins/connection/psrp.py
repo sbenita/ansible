7688f354fbc39847eb2d51451d0ed32e99f8bf09
Shlomi Benita <45397009+sbenita@users.noreply.github.com>
2021-05-21 18:03:56 +0300

    Add ReadTimeout as another exception in connection

--- lib/ansible/plugins/connection/psrp.py
+++ lib/ansible/plugins/connection/psrp.py
@@ -325,7 +325,7 @@ try:
     from pypsrp.powershell import PowerShell, RunspacePool
     from pypsrp.shell import Process, SignalCode, WinRS
     from pypsrp.wsman import WSMan, AUTH_KWARGS
-    from requests.exceptions import ConnectionError, ConnectTimeout
+    from requests.exceptions import ConnectionError, ConnectTimeout, ReadTimeout
 except ImportError as err:
     HAS_PYPSRP = False
     PYPSRP_IMP_ERR = err
@@ -399,7 +399,7 @@ class Connection(ConnectionBase):
                     "psrp connection failure during runspace open: %s"
                     % to_native(e)
                 )
-            except (ConnectionError, ConnectTimeout) as e:
+            except (ConnectionError, ConnectTimeout, ReadTimeout) as e:
                 raise AnsibleConnectionFailure(
                     "Failed to connect to the host via PSRP: %s"
                     % to_native(e)
